a6050851b34257b1a2e1afc0d3fbc7ba0ee37b5c
pilar <pilartandil10@gmail.com>
2023-05-24 13:04:35 -0300

    terminado

---
+++ java.js
@@ -0,0 +1,7 @@
+const boton=document.getElementById("boton")
+boton.addEventListener("click", function(){
+  let username=document.getElementById("username").value
+  console.log (username)
+  let password=document.getElementById("password").value
+  console.log (password)
+})
